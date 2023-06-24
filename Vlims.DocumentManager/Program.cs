af1599bd4dc26ddb17a1223fbf9e8903088e5620
HP <HP@DESKTOP-RJCK03U>
2023-06-24 13:41:46 +0530

    Document Manager API updates

---
+++ Vlims.DocumentManager/Program.cs
@@ -0,0 +1,30 @@
+using Vlims.DocumentManager.Manager;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddScoped<IDocumentManagerService, DocumentManagerService>();
+builder.Services.AddScoped<IAdditionalTaskService, AdditionalTaskService>();
+builder.Services.AddScoped<IDocumentEffectiveService, DocumentEffectiveService>();
+builder.Services.AddScoped<IDocumentPreparationService, DocumentPreparationService>();
+//builder.Services.AddScoped<IDocumentrequestService, DocumentrequestService>();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
