b9b5061d8885d201e5e4722f83edc092e4c8541c
umraz <HP@DESKTOP-NOVEC8T>
2023-07-30 21:01:36 +0530

    geteffectivechanges

--- Vlims.Database/dbo/Stored Procedures/USP_DocumentEffective_PSY_GET.sql
+++ Vlims.Database/dbo/Stored Procedures/USP_DocumentEffective_PSY_GET.sql
@@ -2,20 +2,10 @@
  AS 
  BEGIN 
  BEGIN TRY 
-  SELECT DEID_PSY,
-Documentmanagerid_PSY,
-documenttitle_PSY,
-documentno_PSY,
-documenttype_PSY,
-department_PSY,
-document_PSY,
-EffectiveDate_PSY,
-Reviewdate_PSY,
-CreatedBy_PSY,
-CreatedDate_PSY,
-ModifiedBy_PSY,
-ModifiedDate_PSY 
-  FROM [dbo].[DocumentEffective_PSY] WITH (NOLOCK) where [DEID_PSY] = @DEID_PSY   
+  SELECT DE.*,DP.template_PSY
+  FROM [dbo].[DocumentEffective_PSY] DE WITH (NOLOCK) 
+  JOIN dbo.DocumentPreparation_PSY DP ON DP.DPNID_PSY=DE.Documentmanagerid_PSY
+  where [DEID_PSY] = @DEID_PSY   
  END TRY 
  BEGIN CATCH 
   SELECT ERROR_MESSAGE(); 
