8ff069ca4a06ea1126c5c3ab46022ebcf91e949c
HP <HP@DESKTOP-RJCK03U>
2023-08-06 11:58:18 +0530

    aaaaaaaaaaa

--- Vlims.Database/dbo/Tables/WorkflowUsersMapping.sql
+++ Vlims.Database/dbo/Tables/WorkflowUsersMapping.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].[WorkflowUsersMapping]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [UserId] INT NULL, 
-    [UserName] NVARCHAR(200) NULL, 
-    [WorkFlowId] INT NULL, 
-    [WorkFlowName] NVARCHAR(200) NULL, 
-    [Type] NVARCHAR(100) NULL, 
-    CONSTRAINT [PK_WorkflowReviewMapping] PRIMARY KEY ([Id])
-)
+﻿--CREATE TABLE [dbo].[WorkflowUsersMapping]
+--(
+--	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+--    [UserId] INT NULL, 
+--    [UserName] NVARCHAR(200) NULL, 
+--    [WorkFlowId] INT NULL, 
+--    [WorkFlowName] NVARCHAR(200) NULL, 
+--    [Type] NVARCHAR(100) NULL, 
+--    CONSTRAINT [PK_WorkflowReviewMapping] PRIMARY KEY ([Id])
+--)
