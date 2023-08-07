445a0c58f13f83d9ea3f99259589123508c91e0e
HP <HP@DESKTOP-RJCK03U>
2023-08-07 06:31:26 +0530

    DB build errors

--- Vlims.Database/dbo/Stored Procedures/USP_ExistingDocumentRequest_PSY_GET.sql
+++ Vlims.Database/dbo/Stored Procedures/USP_ExistingDocumentRequest_PSY_GET.sql
@@ -1,24 +1,31 @@
-﻿  CREATE PROCEDURE [dbo].[USP_ExistingDocumentRequest_PSY_GET] @EDRId_PSY int 
+﻿
+  CREATE PROCEDURE [dbo].[USP_ExistingDocumentRequest_PSY_GET] @EDRId_PSY int 
  AS 
  BEGIN 
  BEGIN TRY 
   SELECT EDRId_PSY,
-Documentmanagerid_PSY,
 documentno_PSY,
 documenttitle_PSY,
-printtype_PSY,
-noofcopies_PSY,
-workflow_PSY,
-reason_PSY,
-browse_PSY,
+documenttype_PSY,
+department_PSY,
+document_PSY,
 sampletemplate_PSY,
 CreatedBy_PSY,
 CreatedDate_PSY,
 ModifiedBy_PSY,
-ModifiedDate_PSY 
-  FROM [dbo].[ExistingDocumentRequest_PSY] WITH (NOLOCK) where [EDRId_PSY] = @EDRId_PSY   
+ModifiedDate_PSY,
+effectivedate_PSY,
+reviewdate_PSY 
+ ,count(*) over() as TotalRows 
+ FROM [dbo].[ExistingDocumentRequest_PSY] WITH (NOLOCK) 
+ where [EDRId_PSY] = @EDRId_PSY   
  END TRY 
  BEGIN CATCH 
   SELECT ERROR_MESSAGE(); 
  END CATCH 
- END
+ END 
+
+
+GO
+
+
