8e3e710f5e6cc674b969ed082a40e5d6fa7dd6c2
HP <HP@DESKTOP-RJCK03U>
2023-08-24 22:00:08 +0530

    Bug fixes

--- Vlims.Common/DataAccessHelper.cs
+++ Vlims.Common/DataAccessHelper.cs
@@ -30,8 +30,8 @@ namespace Vlims.Common
 
 
             // _database = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase("Data Source=DESKTOP-RJCK03U\\MSSQLSERVER01;Initial Catalog=Vlims;User ID=sa;Password=Passw0rd;Pooling=true;Min Pool Size=0;Max Pool Size=1024");
-            _database = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase("Data Source=172.173.216.235;Initial Catalog=TemplateDB;User ID=V25TechAdmin;Password=Producttechadmin@123;Pooling=true;Min Pool Size=0;Max Pool Size=1024");
-            // _database = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase("Data Source=tcp:vlimsserver.database.windows.net,1433;Initial Catalog=VlimsDMS;User ID=dmsadmin;Password=Passw0rd@123;Pooling=true;Min Pool Size=0;Max Pool Size=1024");
+            //_database = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase("Data Source=172.173.216.235;Initial Catalog=TemplateDB;User ID=V25TechAdmin;Password=Producttechadmin@123;Pooling=true;Min Pool Size=0;Max Pool Size=1024");
+            _database = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase("Data Source=tcp:vlimsserver.database.windows.net,1433;Initial Catalog=VlimsDMS;User ID=dmsadmin;Password=Passw0rd@123;Pooling=true;Min Pool Size=0;Max Pool Size=1024");
             con = _database.CreateConnection();
         }
 
