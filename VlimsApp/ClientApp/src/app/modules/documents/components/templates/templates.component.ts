4962464c12ad41609ee1f702452029f953de1ffa
umraz <HP@DESKTOP-NOVEC8T>
2023-10-18 23:23:27 +0530

    templatepreviewissue

--- VlimsApp/ClientApp/src/app/modules/documents/components/templates/templates.component.ts
+++ VlimsApp/ClientApp/src/app/modules/documents/components/templates/templates.component.ts
@@ -92,8 +92,8 @@ export class TemplatesComponent {
     // this.preparation.CreatedDate=objtemp.CreatedDate;
     // this.preparation.ModifiedDate=objtemp.ModifiedDate;
     // //this.preparation.dpnid = objtemp.
-    this.preparationsvc.previewtemplate(Number.parseInt(objtemp.DTID)).subscribe((data: any) => {
-      
+    this.templatesvc.getTemplate(objtemp.Templatename).subscribe((data: any) => {
+    //this.preparationsvc.previewtemplate(Number.parseInt(objtemp.DTID)).subscribe((data: any) => {
       this.fileBytes = data;
       this.pdfBytes = this.fileBytes;
       this.spinner.hide();
