7196ddad7fca47a2d4455d847bbe05e2c50d9cfb
UmrazShaikMohammad <UmrazShaikMohammad@VSP-J8M0HR3>
2024-03-23 15:39:38 +0530

    test

--- Vlims.Database/dbo/Stored Procedures/USP_Documentrequest_PSY_INSERT.sql
+++ Vlims.Database/dbo/Stored Procedures/USP_Documentrequest_PSY_INSERT.sql
@@ -48,11 +48,11 @@ Workflow_PSY,GUID_DR)
  DECLARE @PARENTGUID_PSY UNIQUEIDENTIFIER
  SET @PARENTGUID_PSY=(SELECT GUID_DR FROM Documentrequest_PSY WHERE DRID_PSY=@ID)
 
- INSERT into workitems_PSY(TaskName_PSY,TaskType_PSY,Stage_PSY,AssignedToGroup_PSY,InitiatedBy_PSY,InitiatedOn_PSY,Status_PSY,DueDate_PSY,RefrenceId_PSY,ActionType_PSY,IsCompleted_PSY,CreatedBy_PSY,RefrenceGuid_PSY, CreatedDate_PSY, ModifiedBy_PSY,ModifiedDate_PSY,RefrenceGuid_PSY)
- SELECT @documenttype_PSY,'Request','Pending',@AssigntoGroup_PSY,@CreatedBy_PSY,GetDate(),@Status_PSY,GetDate(),@ID,WSR.Type,0,WSR.UserName,@PARENTGUID_PSY,GETDATE(),WSR.UserName, GETDATE(),@PARENTGUID_PSY from WorkflowUsersMapping WSR WHERE WSR.WorkFlowName=@Workflow_PSY AND WSR.Type='Review'
+ INSERT into workitems_PSY(TaskName_PSY,TaskType_PSY,Stage_PSY,AssignedToGroup_PSY,InitiatedBy_PSY,InitiatedOn_PSY,Status_PSY,DueDate_PSY,RefrenceId_PSY,ActionType_PSY,IsCompleted_PSY,CreatedBy_PSY,RefrenceGuid_PSY, CreatedDate_PSY, ModifiedBy_PSY,ModifiedDate_PSY)
+ SELECT @documenttype_PSY,'Request','Pending',@AssigntoGroup_PSY,@CreatedBy_PSY,GetDate(),@Status_PSY,GetDate(),@ID,WSR.Type,0,WSR.UserName,@PARENTGUID_PSY,GETDATE(),WSR.UserName, GETDATE() from WorkflowUsersMapping WSR WHERE WSR.WorkFlowName=@Workflow_PSY AND WSR.Type='Review'
 
- INSERT into workitems_PSY(TaskName_PSY,TaskType_PSY,Stage_PSY,AssignedToGroup_PSY,InitiatedBy_PSY,InitiatedOn_PSY,Status_PSY,DueDate_PSY,RefrenceId_PSY,ActionType_PSY,IsCompleted_PSY,CreatedBy_PSY,RefrenceGuid_PSY, CreatedDate_PSY, ModifiedBy_PSY,ModifiedDate_PSY,RefrenceGuid_PSY)
- SELECT @documenttype_PSY,'Request','Pending',@AssigntoGroup_PSY,@CreatedBy_PSY,GetDate(),@Status_PSY,GetDate(),@ID,WSR.Type,0,WSR.UserName,@PARENTGUID_PSY,GETDATE(),WSR.UserName, GETDATE(),@PARENTGUID_PSY from WorkflowUsersMapping WSR WHERE WSR.WorkFlowName=@Workflow_PSY AND WSR.Type='Approve'
+ INSERT into workitems_PSY(TaskName_PSY,TaskType_PSY,Stage_PSY,AssignedToGroup_PSY,InitiatedBy_PSY,InitiatedOn_PSY,Status_PSY,DueDate_PSY,RefrenceId_PSY,ActionType_PSY,IsCompleted_PSY,CreatedBy_PSY,RefrenceGuid_PSY, CreatedDate_PSY, ModifiedBy_PSY,ModifiedDate_PSY)
+ SELECT @documenttype_PSY,'Request','Pending',@AssigntoGroup_PSY,@CreatedBy_PSY,GetDate(),@Status_PSY,GetDate(),@ID,WSR.Type,0,WSR.UserName,@PARENTGUID_PSY,GETDATE(),WSR.UserName, GETDATE() from WorkflowUsersMapping WSR WHERE WSR.WorkFlowName=@Workflow_PSY AND WSR.Type='Approve'
 
  select @ID 
   
