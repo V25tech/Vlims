612e330b5dd1017f77445fb76fc102568e26d6a8
RST Code <sagar.rebba9@gmail.com>
2024-03-31 13:45:46 +0530

    Upcoming Reminder card

--- VlimsApp/ClientApp/src/app/app.component.ts
+++ VlimsApp/ClientApp/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { LoginService } from "./modules/authentication/services/login.service";
 import { CommonService } from './shared/common.service';
 import { ToastrService } from 'ngx-toastr';
 import { ExistingDocumentRequest, RequestContext } from './models/model';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ExistingDocumentRequestService } from './modules/services/existing-document-request.service';
 
 
 @Component({
@@ -16,16 +14,11 @@ export class AppComponent implements OnInit {
   title = 'DocumentManagementSystem';
   isNavOpen: boolean = false;
   isLoggedIn: boolean = false;
-  existingDocDatasource: ExistingDocumentRequest[] = [];
-  @ViewChild('template') template: TemplateRef<HTMLDivElement> | undefined
-  modalRef: BsModalRef | undefined;
   
   constructor(private loginService:LoginService,
     private commonsvc:CommonService,
     private loginsvc:LoginService,
-    private toastr:ToastrService,
-    private modalService: BsModalService,
-    private existingDocumentRequestService: ExistingDocumentRequestService
+    private toastr:ToastrService
     ){
       //this.checkSessionTimeoutPeriodically();
     this.isLoggedIn = this.loginService.isLoggedIn();
@@ -33,9 +26,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loginService.loginStatusChanged$.subscribe((status: boolean) => {
-      this.isLoggedIn = status;
-      if(status)
-        this.existingDocumentAll()
+      this.isLoggedIn = status;      
     });
     this.checkSessionTimeoutPeriodically();    
   }
@@ -62,51 +53,5 @@ export class AppComponent implements OnInit {
         this.loginService.logout();
       }
     }, 30000); // Check every minute (adjust interval as needed)
-  }
-
-  
-  existingDocumentAll(){
-    let objrequest: RequestContext = { PageNumber: 1, PageSize: 50, Id: 0 };
-    this.existingDocumentRequestService.GetExistingDocumentAll(objrequest).subscribe((data: any) =>{
-      //console.log(data?.response); 
-      let filterdbyreviewDate = data?.response.filter((p:any) => p.reviewDate);
-      
-      filterdbyreviewDate = filterdbyreviewDate.filter((item:ExistingDocumentRequest) => {
-        const currentDate = new Date();
-        const reviewDatePlus15Days = new Date();
-        reviewDatePlus15Days.setDate(currentDate.getDate() + 15);
-  
-        //currentDate.setDate(currentDate.getDate() - 10);
-        // Parse reviewDate string to Date object
-        const reviewDate = new Date(item.reviewDate??'');
-  
-        return (reviewDate > currentDate && reviewDate < reviewDatePlus15Days);
-      });
-      filterdbyreviewDate.forEach((item: any) => {    
-        const reviewDate:Date = new Date(item.reviewDate??'');
-        item.reviewCountDownValue = this.calculateDiff(reviewDate)
-      })
-      this.existingDocDatasource = filterdbyreviewDate;
-      console.log(filterdbyreviewDate);
-      this.openViewer()
-    });
-  }
-
-  calculateDiff(dateSent: Date){
-    let currentDate = new Date();
-    dateSent = new Date(dateSent);
-
-    return Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(dateSent.getFullYear(), dateSent.getMonth(), dateSent.getDate()) ) /(1000 * 60 * 60 * 24));
-}
-
-  openViewer(): void {   
-    if (this.template !== undefined && this.existingDocDatasource && this.existingDocDatasource.length>0) { 
-      this.modalRef = this.modalService.show(this.template, { class: 'modal-lg' });    
-    }
-  }
-
-  closeModel() {
-    if (this.modalRef)
-      this.modalRef.hide();
   }
 }
