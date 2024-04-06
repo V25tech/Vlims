7f5ff557aeef223a0ef4adf921b491ed6f182376
Vinith D <Vinith@V25Tech.com>
2024-04-06 17:35:14 +0530

    License key

--- VlimsApp/ClientApp/src/main.ts
+++ VlimsApp/ClientApp/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 import { registerLicense } from '@syncfusion/ej2-base';
 
 
-registerLicense('	Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCe0xwWmFZfVpgc19CY1ZSTWYuP1ZhSXxXdkFjX39ZdHdRRmVUVUA=');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCe0xwWmFZfVpgc19CY1ZSTWYuP1ZhSXxXdkFjX39ZdHdRRmVUVUA=');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
