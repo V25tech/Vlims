1d5ab6bf8e5d326b615ebd1d25237e39c99a43a1
Sheikh <sheikh@V25Tech.com>
2024-04-10 13:08:08 +0530

    printget

--- Vlims.Database/dbo/Stored Procedures/USP_DocumentPrint_PSY_GET_ALL.sql
+++ Vlims.Database/dbo/Stored Procedures/USP_DocumentPrint_PSY_GET_ALL.sql
@@ -23,6 +23,7 @@ DP.Status_PSY,DSP.template_PSY,DP.Refrence_PSY,DP.PrintCopy_PSY,DP.IsActive_PSY,
  FROM [dbo].[DocumentPrint_PSY] DP WITH (NOLOCK)  
  JOIN dbo.DocumentPreparation_PSY DSP ON DSP.GUID_DP=DP.ReferenceGuid_PSY
  JOIN dbo.DocumentEffective_PSY DE ON DE.ReferenceGuid_PSY=DSP.GUID_DP
+ WHERE DP.IsActive_PSY=1
  Order by DP.CreatedDate_PSY desc    
  OFFSET @PageSize * (@PageNumber - 1) ROWS   
   FETCH NEXT @PageSize ROWS ONLY;   
