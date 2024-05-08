185a04b385a400ae554a172ad957b842f5b5c29b
Vinith D <Vinith@V25Tech.com>
2024-05-09 01:06:35 +0530

    Changes of latest issues

--- Vlims.DocumentMaster/Controllers/DocumentTemplateConfigurationController.cs
+++ Vlims.DocumentMaster/Controllers/DocumentTemplateConfigurationController.cs
@@ -697,7 +697,23 @@ namespace Vlims.Controllers
             string path = Path.Combine(currentDirectory, "Logo", template.header);
             string footerpath = Path.Combine(currentDirectory, "Logo", template.footer);
             string dataUri = string.Empty; string dataUri1 = string.Empty;
-            if (System.IO.File.Exists(path))
+            string documentTypeNumber = string.Empty;
+            switch (template.documenttype.ToUpper())
+            {
+                case "STANDARD TESTING SPECIFICATION":
+                    documentTypeNumber = "STS No.";
+                    break;
+                case "STANDARD TESTING PROCEDURE":
+                    documentTypeNumber = "STP No.";
+                    break;
+                case "STANDARD OPERATING PROCEDURE":
+                    documentTypeNumber = "SOP No.";
+                    break;
+                default:
+                    documentTypeNumber = "STP No.";
+                    break;
+            }
+                if (System.IO.File.Exists(path))
             {
                 string base64EncodedImage = Convert.ToBase64String(System.IO.File.ReadAllBytes(path));
                 dataUri = $"data:image/jpeg;base64,{base64EncodedImage}";
@@ -726,7 +742,7 @@ namespace Vlims.Controllers
             //stringBuilder.AppendLine(@"<th class=""tg-c3ow""><img src=""data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'%3e%3cpath fill='%23000000' d='M350.85 129c25.97 4.67 47.27 18.67 63.92 42 14.65 20.67 24.64 46.67 29.96 78 4.67 28.67 4.32 57.33-1 86-7.99 47.33-23.97 87-47.94 119-28.64 38.67-64.59 58-107.87 58-10.66 0-22.3-3.33-34.96-10-8.66-5.33-18.31-8-28.97-8s-20.3 2.67-28.97 8c-12.66 6.67-24.3 10-34.96 10-43.28 0-79.23-19.33-107.87-58-23.97-32-39.95-71.67-47.94-119-5.32-28.67-5.67-57.33-1-86 5.32-31.33 15.31-57.33 29.96-78 16.65-23.33 37.95-37.33 63.92-42 15.98-2.67 37.95-.33 65.92 7 23.97 6.67 44.28 14.67 60.93 24 16.65-9.33 36.96-17.33 60.93-24 27.98-7.33 49.96-9.67 65.94-7zm-54.94-41c-9.32 8.67-21.65 15-36.96 19-10.66 3.33-22.3 5-34.96 5l-14.98-1c-1.33-9.33-1.33-20 0-32 2.67-24 10.32-42.33 22.97-55 9.32-8.67 21.65-15 36.96-19 10.66-3.33 22.3-5 34.96-5l14.98 1 1 15c0 12.67-1.67 24.33-4.99 35-3.99 15.33-10.31 27.67-18.98 37z'/%3e%3c/svg%3e"" width=""80"" height=""80""></th>");
             //stringBuilder.AppendLine(@"<td class=""tg-0pky""><img src=""https://images.app.goo.gl/v534Zi6U8F7y9KxQA"" width=""140"" height=""80""></td>");
             stringBuilder.AppendLine($@"<th class=""tg-0pky""><img src=""{dataUri}"" width=""140"" height=""80"" /></th>");
-            stringBuilder.AppendLine($"    <th class=\"tg-7btt\" colspan=\"2\">{(template1 != null ? (!string.IsNullOrEmpty(template.titleTable[0][0].inputValue) ? template.titleTable[0][0].inputValue : "test") : "test")}</th>");
+            stringBuilder.AppendLine($"    <th class=\"tg-7btt\" colspan=\"2\">{(template1 != null ? (!string.IsNullOrEmpty(template.titleTable[0][0].inputValue) ? template.titleTable[0][0].inputValue.Replace("\n","<br>") : "test") : "test")}</th>");
             stringBuilder.AppendLine($@"<th class=""tg-0pky""><img src=""{dataUri1}"" width=""100"" height=""80"" style=""align:center"" /></th>");
 
             //stringBuilder.AppendLine(@"<th class=""tg-7btt"" colspan=""3"">ACCENT PHARMACEUTICALS &amp; DIAGNOSTICS<br>FOREST ROAD SALON, H.P. (INDIA)<br>QUALITY CONTROL DEPARTMENT<br>STANDARD TESTING PROCEDURE</th>");
@@ -741,7 +757,7 @@ namespace Vlims.Controllers
             stringBuilder.AppendLine(@"</tr>");
 
             stringBuilder.AppendLine(@"<tr>");
-            stringBuilder.AppendLine(@"<td class=""tg-1wig"">STP No.</td>");
+            stringBuilder.AppendLine($"<td class=\"tg-1wig\">{(documentTypeNumber)}</td>");
             //stringBuilder.AppendLine(@"<td class=""tg-0pky"">STP NO. 1234</td>");
             stringBuilder.AppendLine($"    <td class=\"tg-1wig\">{(template1 != null ? (!string.IsNullOrEmpty(template1.DocumentNo) ? template1.DocumentNo : "test") : "test")}</td>");
             stringBuilder.AppendLine(@"<td class=""tg-1wig"">Revision No.</td>");
